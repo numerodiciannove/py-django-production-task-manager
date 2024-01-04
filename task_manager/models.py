97f9ff30cae9b73720790e59eda04c9207e87d20
Roman Muraviov <5079447@gmail.com>
2024-01-04 18:57:03 +0200

    add new models for 'task_manager'

--- task_manager/models.py
+++ task_manager/models.py
@@ -1,3 +1,79 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
-# Create your models here.
+
+class Position(models.Model):
+    name = models.CharField(
+        max_length=100, unique=True,
+    )
+
+
+class TaskType(models.Model):
+    name = models.CharField(
+        max_length=100,
+    )
+
+
+class Task(models.Model):
+    PRIORITY_CHOICES = [
+        ("urgent", "Urgent"),
+        ("height", "Height"),
+        ("medium", "Medium"),
+        ("low", "Low"),
+    ]
+    name = models.CharField(max_length=255, )
+    description = models.TextField()
+    deadline = models.DateField()
+    is_complete = models.BooleanField(default=False)
+    task_type = models.ForeignKey(TaskType, on_delete=models.CASCADE)
+    priority = models.CharField(
+        max_length=10,
+        choices=PRIORITY_CHOICES,
+    )
+
+    class Meta:
+        ordering = ["name"]
+
+
+class Worker(AbstractUser):
+    position = models.ForeignKey(
+        Position,
+        on_delete=models.CASCADE,
+        null=True
+    )
+    phone_number = models.CharField(
+        max_length=13,
+        unique=True,
+        help_text="For example '+380951911919'. Without quotes"
+    )
+
+    def __str__(self):
+        return (
+            f"{self.username}: "
+            f"({self.first_name} {self.last_name}) {self.position}"
+        )
+
+
+class Project(models.Model):
+    name = models.CharField(max_length=50, unique=True)
+    tasks = models.ManyToManyField(
+        Task,
+        related_name="projects",
+    )
+
+    class Meta:
+        ordering = ["name", ]
+
+    def __str__(self):
+        return f"{self.name}: ({self.tasks})"
+
+
+class Team(models.Model):
+    name = models.CharField(max_length=100, unique=True)
+    projects = models.ManyToManyField(
+        Project,
+        related_name="teams",
+    )
+
+    class Meta:
+        ordering = ["name", ]
