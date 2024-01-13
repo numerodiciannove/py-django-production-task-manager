86b7e4ff389a47cc738f4dbd0e342eadfcbca15b
Roman Muraviov <5079447@gmail.com>
2024-01-13 20:10:16 +0200

    Add detail view for "projects" with template, template includes (task_accordion.html, js) url, and other. Add utils.py with "function calculate_percentage" for views.

--- task_manager/urls.py
+++ task_manager/urls.py
@@ -1,7 +1,7 @@
 from django.conf import settings
 from django.conf.urls.static import static
-from django.urls import path, include
-from task_manager.views import index, ProjectListView
+from django.urls import path
+from task_manager.views import index, ProjectListView, ProjectDetailView
 
 urlpatterns = [
     path(
@@ -12,6 +12,10 @@ urlpatterns = [
         ProjectListView.as_view(),
         name="projects-list",
     ),
+    path(
+        "projects/<int:pk>/",
+        ProjectDetailView.as_view(),
+        name="project-detail"),
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
 
 app_name = "task_manager"
--- task_manager/views.py
+++ task_manager/views.py
@@ -1,8 +1,8 @@
-from typing import Dict, Any
-
+from django.db.models import QuerySet
 from django.shortcuts import render
+from django.urls import reverse
 from django.views import generic
-
+from .utils import calculate_percentage
 from task_manager.models import Task, Worker, Project, Team, Position
 
 
@@ -16,10 +16,15 @@ def index(request):
     num_completed_projects = Project.objects.filter(is_complete=True).count()
     num_incomplete_tasks = Task.objects.filter(is_complete=False).count()
     num_completed_tasks = Task.objects.filter(is_complete=True).count()
-    percent_tasks_completed = (num_completed_tasks / num_tasks) * 100
-    percent_projects_completed = (num_completed_projects / num_projects) * 100
     num_visits = request.session.get("num_visits", 0)
     request.session["num_visits"] = num_visits + 1
+
+    percent_tasks_completed = calculate_percentage(
+        num_completed_tasks, num_tasks
+    )
+    percent_projects_completed = calculate_percentage(
+        num_completed_projects, num_projects
+    )
 
     cards = [
         {
@@ -76,50 +81,72 @@ def index(request):
 class ProjectListView(generic.ListView):
     model = Project
     context_object_name = "project_list"
-    template_name = "task_manager/projects_list.html"
+    template_name = "task_manager/projects/projects_list.html"
 
-    def get_queryset(self):
+    def get_queryset(self) -> QuerySet[Project]:
         return Project.objects.all()
 
-    def get_context_data(self, **kwargs):
+    def get_context_data(self, **kwargs) -> dict:
         context = super().get_context_data(**kwargs)
         project_list = Project.objects.prefetch_related("tasks").all()
         num_completed_projects = project_list.filter(is_complete=True).count()
         num_projects = project_list.count()
 
-        if num_projects > 0:
-            percent_projects_completed = (
-                                                     num_completed_projects / num_projects) * 100
-        else:
-            percent_projects_completed = 0
+        percent_projects_completed = calculate_percentage(
+            num_completed_projects, num_projects
+        )
 
         context["project_list"] = project_list
         context["percent_projects_completed"] = percent_projects_completed
+        context["projects_info"] = self.get_projects_info(project_list)
 
         return context
 
+    @staticmethod
+    def get_projects_info(project_list) -> list:
+        tasks_info = []
+        for project in project_list:
+            project_info = {
+                "project_name": project.name,
+                "tasks_info": [(task.name, task.is_complete) for task in
+                               project.tasks.all()],
+                "id": project.id,
+                "task_names": [],
+                "deadlines": [],
+                "priorities": []
+            }
+            for task in project.tasks.all():
+                project_info["task_names"].append(task.name)
+                project_info["deadlines"].append(task.deadline)
+                project_info["priorities"].append(task.priority)
+            tasks_info.append(project_info)
+        return tasks_info
 
-class TaskListView(generic.ListView):
-    model = Task
-    context_object_name = "task_list"
-    template_name = "task_manager/task_list.html"
 
-    def get_queryset(self):
-        return Project.objects.all()
+class ProjectDetailView(generic.DetailView):
+    model = Project
+    template_name = 'task_manager/projects/project_detail.html'
 
     def get_context_data(self, **kwargs):
-        project_list = Project.objects.prefetch_related("tasks").all()
-        num_completed_projects = project_list.filter(is_complete=True).count()
-        num_projects = project_list.count()
-
-        if num_projects > 0:
-            percent_projects_completed = (
-                            num_completed_projects / num_projects
-                            ) * 100
-        else:
-            percent_projects_completed = 0
-
-        context["project_list"] = project_list
-        context["percent_projects_completed"] = percent_projects_completed
-
+        context = super().get_context_data(**kwargs)
+        tasks_list = self.object.tasks.all()
+        context["project_workers"] = self.object.teams.first().workers.all()
+        context["project_tasks"] = self.get_tasks_info(tasks_list)
         return context
+
+    @staticmethod
+    def get_tasks_info(tasks_list) -> list:
+        tasks_info = []
+        for task in tasks_list:
+            task_info = {
+                "id": task.id,
+                "name": task.name,
+                "description": task.description,
+                "start_time": task.start_time,
+                "is_complete": task.is_complete,
+                "deadline": task.deadline,
+                "priority": task.priority,
+                "task_types": task.task_type.all()
+            }
+            tasks_info.append(task_info)
+        return tasks_info
