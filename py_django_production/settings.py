828f759c0f341c6315640fe7c3c73cecbb75525e
Roman Muraviov <5079447@gmail.com>
2024-01-24 21:06:00 +0200

    update settings, urls, requirements.txt

--- py_django_production/settings.py
+++ py_django_production/settings.py
@@ -19,12 +19,15 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-#l&!7t#45@i(8%s^r8@&_n@dz4wb5rt(f9gj8)m*8-vat5y1zc"
+SECRET_KEY = os.environ.get(
+    "DJANGO_SECRET_KEY",
+    "django-insecure-#l&!7t#45@i(8%s^r8@&_n@dz4wb5rt(f9gj8)m*8-vat5y1zc"
+)
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.environ.get("DJANGO_DEBUG", "") != "False"
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["127.0.0.1", ]
 
 # Application definition
 
